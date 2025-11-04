2f211c7a206194888e72eb0171978f579af4f0c3
parth a <111271071+georgemonkey@users.noreply.github.com>
2025-11-04 10:45:16 -0800

    redid telemetry

--- src/op_control.cpp
+++ src/op_control.cpp
@@ -8,11 +8,6 @@
 #include <string>
 
 //STATES
-//stalling
-bool frontIntakeStall = false;
-bool colorSortRollerStall = false;
-bool middleRollersStall = false;
-bool scoringRollerStall = false;
 //extensions
 bool isScoringBarUp = false;
 bool isLoaderExtended = false;
@@ -20,80 +15,6 @@ bool isWingsOut = false;
 
 //core func
 bool isIntakeOn = false;
-bool shouldSC = false;
-
-
-//amp rpm vars
-double colorSortRoller_rpm = 0.0, colorSortRoller_current = 0.0;
-double frontIntake_rpm = 0.0, frontIntake_current = 0.0;
-double middleRollers_rpm = 0.0, middleRollers_current = 0.0;
-double scoringRoller_rpm = 0.0, scoringRoller_current = 0.0;
-
-//drivetrain storage
-double leftDT_rpm[3] = {0}, leftDT_current[3] = {0};
-double rightDT_rpm[3] = {0}, rightDT_current[3] = {0};
-pros::Mutex log_mutex;
-std::ofstream logFile;
-bool logInitialized = false;
-
-void initLog() {
-  if (!logInitialized) {
-    logFile.open("/usd/stall_log.csv", std::ios::out | std::ios::trunc);
-    if (logFile.is_open()) {
-      logFile << "time_ms,"
-              << "frontIntake_rpm,frontIntake_current,frontIntakeStall,"
-              << "colorSortRoller_rpm,colorSortRoller_current,colorSortRollerStall,"
-              << "middleRollers_rpm,middleRollers_current,middleRollersStall,"
-              << "scoringRoller_rpm,scoringRoller_current,scoringRollerStall,"
-              // drivetrain motors
-              << "leftDT_m7_rpm,leftDT_m7_amp,"
-              << "leftDT_m6_rpm,leftDT_m6_amp,"
-              << "leftDT_m5_rpm,leftDT_m5_amp,"
-              << "rightDT_m14_rpm,rightDT_m14_amp,"
-              << "rightDT_m15_rpm,rightDT_m15_amp,"
-              << "rightDT_m16_rpm,rightDT_m16_amp"
-              << std::endl;
-      logInitialized = true;
-    }
-  }
-}
-
-void logData() {
-  if (logInitialized && logFile.is_open()) {
-    log_mutex.take();
-
-    // collect drivetrain data
-    leftDT_rpm[0] = pros::Motor(-7).get_actual_velocity();
-    leftDT_current[0] = pros::Motor(-7).get_current_draw();
-    leftDT_rpm[1] = pros::Motor(-6).get_actual_velocity();
-    leftDT_current[1] = pros::Motor(-6).get_current_draw();
-    leftDT_rpm[2] = pros::Motor(5).get_actual_velocity();
-    leftDT_current[2] = pros::Motor(5).get_current_draw();
-
-    rightDT_rpm[0] = pros::Motor(14).get_actual_velocity();
-    rightDT_current[0] = pros::Motor(14).get_current_draw();
-    rightDT_rpm[1] = pros::Motor(15).get_actual_velocity();
-    rightDT_current[1] = pros::Motor(15).get_current_draw();
-    rightDT_rpm[2] = pros::Motor(-16).get_actual_velocity();
-    rightDT_current[2] = pros::Motor(-16).get_current_draw();
-
-    logFile << pros::millis() << ","
-            << frontIntake_rpm << "," << frontIntake_current << "," << frontIntakeStall << ","
-            << colorSortRoller_rpm << "," << colorSortRoller_current << "," << colorSortRollerStall << ","
-            << middleRollers_rpm << "," << middleRollers_current << "," << middleRollersStall << ","
-            << scoringRoller_rpm << "," << scoringRoller_current << "," << scoringRollerStall << ","
-            // drivetrain
-            << leftDT_rpm[0] << "," << leftDT_current[0] << ","
-            << leftDT_rpm[1] << "," << leftDT_current[1] << ","
-            << leftDT_rpm[2] << "," << leftDT_current[2] << ","
-            << rightDT_rpm[0] << "," << rightDT_current[0] << ","
-            << rightDT_rpm[1] << "," << rightDT_current[1] << ","
-            << rightDT_rpm[2] << "," << rightDT_current[2]
-            << std::endl;
-
-    log_mutex.give();
-  }
-}
 
 // drive mode handler
 void handleDriveMode(bool driveMode) {
@@ -120,7 +41,6 @@ void handleIOCommands() {
   }
 
   if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) { // when L2 is held the system reverses when let go it returns to the state of L1 toggle
-    shouldSC = false;
     frontIntake.move(-127);
     middleRollers.move(-127);
     scoringRoller.move(-127);
@@ -128,7 +48,6 @@ void handleIOCommands() {
   }
 
   if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) { // when R1 is held the system runs forward with scoring bar deployed when let go returns to the state of L1 toggle
-    shouldSC = false;
     scoringBar.set_value(true);
     frontIntake.move(127);
     middleRollers.move(127);
@@ -139,7 +58,6 @@ void handleIOCommands() {
   }
 
   if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) { // when R2 is held the system runs forward with scoring roller reversed for mid scoring and when let go returns to the state of L1 toggle
-    shouldSC = false;
     scoringBar.set_value(false);
     frontIntake.move(127);
     middleRollers.move(100);
@@ -148,22 +66,10 @@ void handleIOCommands() {
   }
 
   if (isIntakeOn) { // if the L1 is toggled on then this passes
-    shouldSC = true;
     frontIntake.move(127);
     middleRollers.move(127);
     scoringRoller.move(127);
-    if (middleRollersStall) {
-      middleRollers.move(20);
-    } else {
-      middleRollers.move(127);
-    }
-    if (scoringRollerStall) {
-      scoringRoller.move(20);
-    } else {
-      scoringRoller.move(127);
-    }
   } else { // if the L1 is toggled off then this passes
-    shouldSC = false;
     frontIntake.move(0);
     middleRollers.move(0);
     scoringRoller.move(0);
@@ -171,46 +77,24 @@ void handleIOCommands() {
 }
 
 void telemetry() {
-  initLog(); //initalizes the sd card for data input
-  //updates variables every time the function is called
-  colorSortRoller_rpm = colorSortRoller.get_actual_velocity();
-  colorSortRoller_current = colorSortRoller.get_current_draw();
-  frontIntake_rpm = frontIntake.get_actual_velocity();
-  frontIntake_current = frontIntake.get_current_draw();
-  middleRollers_rpm = middleRollers.get_actual_velocity();
-  middleRollers_current = middleRollers.get_current_draw();
-  scoringRoller_rpm = scoringRoller.get_actual_velocity();
-  scoringRoller_current = scoringRoller.get_current_draw();
+  std::ofstream file("/usd/log.csv", std::ios::app);
+  if (!file.is_open()) return;
 
-  //every time the intake sequence is toggled these flaggers reset
-  //higher the rpm thresh. the more sensitive
-  if (shouldSC){ //if the stall check code is permitted to run, set by other parts in the code this passes
-    if (scoringRoller_rpm < 20){
-      scoringRollerStall = true;
-    }
-    else if (scoringRoller_rpm > 40){
-      scoringRollerStall = false;
-    }
-    if (middleRollers_rpm < 20){
-    middleRollersStall = true;
-    } 
-    else if (middleRollers_rpm > 40){
-      middleRollersStall = false;
-    } 
+  // write header if file empty
+  if (file.tellp() == 0)
+      file << "time,x,y,theta,psi,descore?,loader?,wings?\n";
 
-    if (frontIntake_rpm < 10){
-    frontIntakeStall = true;
-    } 
-    else if (frontIntake_rpm > 40){
-      frontIntakeStall = false;
-    } 
-  }                                                                                                                   
-  //displays the rpm and the stall state of each roller section on the brains 5,SIX, SEVENNN lines of the screen (o)(o)
-  pros::lcd::print(5, "F RPM: %.2f %s", frontIntake_rpm, frontIntakeStall ? "FRONT FULL" : "");
-  pros::lcd::print(6, "M RPM: %.2f %s", middleRollers_rpm, middleRollersStall ? "MIDDLE FULL" : "");
-  pros::lcd::print(7, "S RPM: %.2f %s", scoringRoller_rpm, scoringRollerStall ? "TOP FULL" : "");
-  logData(); //logs the data onto the sd card for data analysis
+  // write one row of data
+  file  << pros::millis() << ","
+        << chassis.getPose().x << ","
+        << chassis.getPose().y << ","
+        << chassis.getPose().theta << ","
+        << isScoringBarUp << ","
+        << isLoaderExtended << ","
+        << isWingsOut << "\n";
+  file.close();
 }
+
 
 void handleLoaderMechCommands() { //toggle button for loader mech
   if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_LEFT)) { //if the controller recognizes a new press from the left arrow button
