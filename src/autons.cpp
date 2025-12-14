34c30c2039f8139fcafc47c2965450f05c7329fd
parth a <111271071+georgemonkey@users.noreply.github.com>
2025-12-13 22:33:21 -0800

    comments

--- src/autons.cpp
+++ src/autons.cpp
@@ -582,47 +582,64 @@ void two_goal_RIGHT(){
 };
 
 void one_goal_left() {
+    // sets position to top left of park zone facing forwards
     chassis.setPose(-16.5, -48, 0);
 
+    // keeps the scoring bar closed
     scoringBar.set_value(true);
+    // starts intake system
     frontIntake.move(127);
-    colorSortRoller.move(127);
     middleRollers.move(127);
-    scoringRoller.move(20);
+    scoringRoller.move(100);
+    
+    // keeps the wing down
     wingMech.set_value(false);
 
+    // moves to 3 cluster and loader down after 600 ms
     chassis.moveToPose(-24, -24, -15, 3000, {.maxSpeed = 80, .minSpeed = 40});
     pros::delay(600);
-
     loaderMech.set_value(true);
 
+    // faces to goal + loader alley
     chassis.turnToPoint(-48, -45, 800);
 
+    // enters the loader using motion chain 
     chassis.moveToPose(-47, -48, 180, 1500, {.lead = 0.3, .minSpeed = 60, .earlyExitRange = 5});
-
+    // moves deeper to collect
     chassis.moveToPoint(-47, -64, 1400, {.minSpeed = 60});
     pros::delay(150);
 
+    // verifies that scoring bar is closed
     scoringBar.set_value(true);
 
+    // moves back into the goal waits to reach for second then proceeds
     chassis.moveToPoint(-48, -24, 1500, {.forwards = false});
     pros::delay(1000);
 
+    // sets colorsort to off
     colorsortOn = false;
-    scoringRoller.move(127);
+
+    // open scoring bar for 1.4 seconds
     scoringBar.set_value(false);
     pros::delay(1400);
 
+    // closes scoring bar
     scoringBar.set_value(true);
+
+    // shuts intake system off
     scoringRoller.move(0);
     middleRollers.move(0);
-    colorsortOn = true;
     frontIntake.move(0);
+    // colorsort switched back on for remainder of match
+    colorsortOn = true;
 
+    // moves in front of goal
     chassis.moveToPoint(-48, -50, 1000);
 
+    // wings the goal
     chassis.moveToPose(-36, -8, 0, 2500, {.forwards = false,.lead=-0.6}, false);
 
+    // verifies the bot doesnt get pushed
     while (chassis.getPose().y < -10) {
         chassis.moveToPose(-36.5, -8, 180, 2500, {.forwards = false,.lead=-0.6});
     }
