0e34c2f57761b0ef7859a70a3e04bd5ddae1fcfa
parth a <111271071+georgemonkey@users.noreply.github.com>
2026-01-01 13:59:59 -0800

    awp mid goal correction

--- src/routes/awp.cpp
+++ src/routes/awp.cpp
@@ -19,7 +19,7 @@ void solo_awp(){
     
     chassis.setPose(16,-48,90);
     chassis.moveToPoint(40,-48,800,{.minSpeed=50,.earlyExitRange=2});
-    chassis.moveToPose(45.5,-80,180,1450,{.minSpeed=75},false);
+    chassis.moveToPose(45.5,-80,180,1450,{.minSpeed=65},false);
 
     chassis.moveToPoint(45.5,-22,1200,{.forwards=false},false);
     scoringGate.set_value(false);
@@ -35,16 +35,24 @@ void solo_awp(){
     chassis.turnToHeading(-94,400);
     chassis.moveToPoint(-25.5,-22.5,1100,{.minSpeed=45});
     colorsortOn=false;
+    //mid goal movement
     chassis.moveToPose(-2,-2,225,1200,{.forwards=false,.minSpeed=60},false);
-
+    //mid goal scoring 
+    // reverse to unclog for 200ms
     frontIntake.move(-100);
     middleRollers.move(-127);
     scoringRoller.move(-127);
+
     pros::delay(200);
+
+    // direct into mid goal
     frontIntake.move(100);
     middleRollers.move(127);
     scoringRoller.move(-100);
-    pros::delay(750);
+
+    chassis.moveToPoint(0,0,750,{.forwards=false,.minSpeed=40},false);
+    //pros::delay(750);
+    // spin regularly to continue
     frontIntake.move(0);
     middleRollers.move(-127);
     scoringRoller.move(-127);
@@ -54,7 +62,7 @@ void solo_awp(){
     scoringRoller.move(127);
     frontIntake.move(127);
     middleRollers.move(127);
-    chassis.moveToPose(-45,-90,180,1000,{.minSpeed=70},false);
+    chassis.moveToPose(-45,-90,180,1000,{.minSpeed=60},false);
 
     chassis.moveToPoint(-46,-25,1100,{.forwards=false},false);
     scoringGate.set_value(false);
