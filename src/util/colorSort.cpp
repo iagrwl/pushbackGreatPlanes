28fbac92ac05a907551fe10834e140907fce1c8e
parth a <111271071+georgemonkey@users.noreply.github.com>
2026-01-25 17:47:00 -0800

    daddy color

--- src/util/colorSort.cpp
+++ src/util/colorSort.cpp
@@ -12,91 +12,40 @@ int hue;
 int proximity;
 
 
-// void colorSort() {
-//     // gets current time from the program
-//     current = pros::millis();
-//     // gets proximity from sensor
-//     proximity = topOptical.get_proximity();
-//     topOptical.set_integration_time(3);
-//     pros::lcd::print(0, "Proximity: %d", topOptical.get_proximity());
-//     //opponent color
-//     if (isRed) {
-//         oppMin = BLUE_MIN;
-//         oppMax = BLUE_MAX;
-//     } else {
-//         oppMin = RED_MIN;
-//         oppMax = RED_MAX;
-//     }
-
-
-
-
-//     // if block is detected in the intake
-//     if (proximity>240&&proximity<260){
-//         // gets current hue from sensor
-//         hue = topOptical.get_hue();
-//     // if hue is detected as in opponents color range and sort is false
-//     if (hue>=oppMin&&hue<=oppMax&&!sort){
-//         // sets sorting to true
-//         sort = true;
-//         // sets var start to the time that the opponent block is detected
-//         start = pros::millis();
-//     }
-//     // if the sorting has been going on for less than 300ms
-//     if (current-start<300&&sort)
-//     {
-//         scoringRoller.move(-127);
-//         middleRollers.move(80);
-//         frontIntake.move(127);
-//     }
-//     // if the sorting has been going on for longer than 300ms it lets the driver code continue operating rollers and sets sort to false
-//     if (current-start>300){
-//         scoringRoller.move(-127);
-//         pros::delay(100);
-//         scoringRoller.move(0);
-//         start=0;
-//         sort=false;
-//     }
-//     }
-    
-    
-      
-
-// }
-
 void colorSort() {
     
-    // fastest int time to 3ms
+    // Set integration time to minimum for fastest response (3ms)
     topOptical.set_integration_time(3);
-    proximity = topOptical.get_proximity();
-// all code that should run consistently below here
     
-    // assigns enemy range based on alliance color
-    if (isRed){
+    
+        
+            if (isRed){
         oppMin = BLUE_MIN;
         oppMax = BLUE_MAX;
-    }
-    else{
-        oppMin = RED_MIN;
-        oppMax = RED_MAX;
-    }
-    // checks if colorsort is allowed to run
-    if (colorsortOn == true){
-    if (proximity>240&&proximity<260){
-        
-        // gets hue from sensor
+        }
+        else{
+            oppMin = RED_MIN;
+            oppMax = RED_MAX;
+        }
+        if (colorsortOn == true){
+        // Get current color sensor reading (hue value)
         double colorValue = topOptical.get_hue();
-    
-        // filters false readings
+        
+        // Check if the reading is valid (not an error)
         if (colorValue >= 0 && colorValue <= 360) {
-            // checks if in range of enemy color
+            // Check if detected color is within opposite alliance range
             if (colorValue >= oppMin && colorValue <= oppMax) {
-                // code that occurs when enemy color detected
-                scoringRoller.move(-120);
-            
+                // Opposite alliance color detected - STOP THE ROLLER
+                
+                scoringRoller.move(-50);
+                
             }
         }
-    }
-}
+        }
+        else{
+            pros::delay(5);
+        }
+        // Minimal delay for fastest detection
+        pros::delay(2);
     
 }
