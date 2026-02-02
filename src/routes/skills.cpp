bad4c5c0357c6b167935756e12b4f0e902ee748e
BhargavN15 <163083131+BhargavN15@users.noreply.github.com>
2026-02-01 22:41:58 -0800

    simple skills

--- src/routes/skills.cpp
+++ src/routes/skills.cpp
@@ -28,8 +28,7 @@ void autonSkills() {
     frontIntake.move(127);
     middleRollers.move(127);
     scoringRoller.move(127);
-
-    chassis.setPose(0, -47, 180);
+    chassis.setPose(0, 0, 180);
 
     // =========================
     // CLEAR STARTING ZONE
@@ -93,12 +92,12 @@ void autonSkills() {
     chassis.turnToPoint(0, 0, 700,
         {.forwards = false, .minSpeed = 60, .earlyExitRange = 4});
 
-    chassis.moveToPoint(-10, -10.5, 3000,
+    chassis.moveToPoint(-10, -10, 3000,
         {.forwards = false, .maxSpeed = 50});
 
-    pros::delay(400);
+    pros::delay(1000);
     scoreMidGoal();
-    pros::delay(2000);
+    pros::delay(3000);
 
     // =========================
     // FIRST LOADER COLLECTION
@@ -115,52 +114,33 @@ void autonSkills() {
 
     wallDistance(true, true);
 
-    chassis.moveToPoint(-47.5, -70, 1500,
-        {.maxSpeed = 75}, false);
-
+    chassis.moveToPoint(-46, -72, 1500, {.maxSpeed = 70}, false);
     left_dt.move(-40);
     right_dt.move(-40);
     pros::delay(200);
-
     left_dt.move(90);
     right_dt.move(90);
     pros::delay(800);
-    pros::delay(500);
 
-    wallDistance(true, true);
-
-    // =========================
-    // SCORE FIRST LONG GOAL
-    // =========================
-    chassis.moveToPoint(-48, -53, 1000,
-        {.forwards = false, .minSpeed = 60, .earlyExitRange = 5});
-
-    chassis.turnToPoint(-60, -37, 1500,
-        {.minSpeed = 60, .earlyExitRange = 5});
-
+    // go through alley
+    chassis.moveToPoint(-46, -48, 1200, {.forwards = false});
     loaderMech.set_value(false);
+    //chassis.turnToPoint(-60, -30, 750);
+    chassis.moveToPoint(-61, -36, 1500, {.minSpeed = 40, .earlyExitRange = 5});
+    frontIntake.move(0);
 
-    chassis.moveToPoint(-60, -37, 1500,
-        {.minSpeed = 60, .earlyExitRange = 5});
-
-    chassis.turnToHeading(0, 1500,
-        {.minSpeed = 60, .earlyExitRange = 5});
-
-    chassis.moveToPoint(-60, 34, 2000,
+    chassis.moveToPoint(-61, 35, 2000,
         {.minSpeed = 60, .earlyExitRange = 8});
 
     chassis.turnToPoint(-50, 24, 1500,
         {.forwards = false, .minSpeed = 60, .earlyExitRange = 3});
 
     chassis.moveToPose(-51, 18, 0, 2000,
-        {.forwards = false, .lead = 0.4, .minSpeed = 60, .earlyExitRange = 5}, false);
-
+        {.forwards = false, .lead = 0.5, .minSpeed = 60, .earlyExitRange = 5}, false);
+    left_dt.move(-40);
+    right_dt.move(-40);
     loaderMech.set_value(true);
 
-    // Score
-    frontIntake.move(-127);
-    middleRollers.move(-127);
-    scoringRoller.move(-127);
     pros::delay(250);
 
     scoringGate.set_value(false);
@@ -173,12 +153,13 @@ void autonSkills() {
     wallDistance(true, false);
     chassis.setPose(chassis.getPose().x, 28, chassis.getPose().theta);
     pros::delay(1000);
-
+    scoringGate.set_value(true);
     // =========================
     // SECOND LOADER + SCORE
     // =========================
-    chassis.moveToPoint(-47.5, 70, 1800,
+    chassis.moveToPoint(-48, 70, 1800,
         {.maxSpeed = 75}, false);
+    
 
     left_dt.move(-40);
     right_dt.move(-40);
@@ -189,17 +170,11 @@ void autonSkills() {
     pros::delay(800);
     pros::delay(500);
 
-    scoringGate.set_value(true);
-
     chassis.moveToPoint(-48.5, 23, 2500,
         {.forwards = false, .minSpeed = 80});
 
     loaderMech.set_value(false);
-
-    frontIntake.move(-127);
-    middleRollers.move(-127);
-    scoringRoller.move(-127);
-    pros::delay(200);
+    pros::delay(900);
 
     frontIntake.move(127);
     middleRollers.move(127);
@@ -217,44 +192,91 @@ void autonSkills() {
     scoringGate.set_value(true);
     chassis.moveToPoint(-48, 20, 750,
         {.forwards = false, .maxSpeed = 40});
-
-    chassis.moveToPoint(-48, 36, 3000, {.maxSpeed=60, .minSpeed=40, .earlyExitRange=5});
+    chassis.moveToPoint(-48, 40, 3000, {.maxSpeed=60, .minSpeed=40, .earlyExitRange=5});
     chassis.turnToHeading(90, 500, {.minSpeed=60, .earlyExitRange=5});
-    chassis.moveToPoint(47.5, 36, 3000, {.maxSpeed=90});
+    chassis.moveToPoint(47, 40, 3000, {.maxSpeed=90});
     chassis.turnToHeading(0, 700, {}, false);
 
     loaderMech.set_value(true); // Prep loader for intake
     wallDistance(true, true);
 
-    chassis.moveToPoint(47.5, 70, 1800, {.maxSpeed=70}, false); // Collect second loader
+    chassis.moveToPoint(46, 72, 1800, {.maxSpeed=70}, false); // Collect second loader
     left_dt.move(-40); right_dt.move(-40); pros::delay(200);
     left_dt.move(90); right_dt.move(90); pros::delay(800);
 
-    chassis.turnToHeading(0, 700, {.minSpeed=60, .earlyExitRange=5}, false);
+    chassis.moveToPoint(48,53,700,{.forwards=false,.minSpeed=60,.earlyExitRange=5});
+    frontIntake.move(127);
+    chassis.turnToPoint(60,37,1500,{.minSpeed=60,.earlyExitRange=5});
     pros::delay(500);
-    left_dt.move(0); right_dt.move(0);
+    loaderMech.set_value(false);
+    chassis.moveToPoint(60,37,1500,{.minSpeed=60,.earlyExitRange=5});
+    chassis.turnToHeading(180,1500,{.minSpeed=60,.earlyExitRange=5});
+    chassis.moveToPoint(60,-39,2300,{.minSpeed=60,.earlyExitRange=8});
 
-    // Score second long goal first time
-    wingMech.set_value(true);
-    scoringGate.set_value(true);
-    chassis.moveToPoint(48, -22, 2200, {.forwards=false, .minSpeed=80, .earlyExitRange=4});
-    pros::delay(800);
-    frontIntake.move(-127); middleRollers.move(-127); scoringRoller.move(-127);
+    //line up to score
+    chassis.turnToPoint(51.5,-24,1500,{.forwards=false,.minSpeed=60,.earlyExitRange=3});
+    frontIntake.move(127);
+    chassis.moveToPose(51.5,-18,180,2000,{.forwards=false,.lead=0.6,.minSpeed=60,.earlyExitRange=5},false);
+    left_dt.move(-40);
+    right_dt.move(-40);
+    loaderMech.set_value(true);
+    //move to collect from 3rd loader
+    //pros::delay(1000);
+    wallDistance(true,false);
+
     pros::delay(300);
-    frontIntake.move(127); middleRollers.move(127); scoringRoller.move(127);
+    scoringGate.set_value(false);
+    frontIntake.move(127);
+    middleRollers.move(127);
+    scoringRoller.move(127);
+    pros::delay(2200);
+    left_dt.move(0);
+    right_dt.move(0);
+
+    //chassis.setPose(chassis.getPose().x,28, chassis.getPose().theta);
+    scoringGate.set_value(true);
+    //chassis.moveToPoint(45.75,70,3000,{.maxSpeed=70});
+    chassis.moveToPoint(47.5,-70,1500,{.maxSpeed=70},false);
+    left_dt.move(-40);
+    right_dt.move(-40);
+    pros::delay(200);
+    left_dt.move(90);
+    right_dt.move(90);
+    pros::delay(800);
+    pros::delay(500);
+
+    //prep to score 2nd time in 2nd long goal
+    wingMech.set_value(true);
+
+
+    chassis.moveToPoint(48,-22,2200,{.forwards=false,.minSpeed=80,.earlyExitRange=4});
+    left_dt.move(-40);
+    right_dt.move(-40);
+
+    //score 3rd time in 2nd long goal
+    pros::delay(900);
+    frontIntake.move(127);
+    middleRollers.move(127);
+    scoringRoller.move(127);
     scoringGate.set_value(false);
     loaderMech.set_value(false);
     pros::delay(1600);
+    left_dt.move(0);
+    right_dt.move(0);
 
-    // ======= PARK =======
-    chassis.moveToPoint(48, -34, 2500, {.minSpeed=127});
+
+    chassis.moveToPoint(48,-34,2500,{.minSpeed=127});
     pros::delay(300);
     scoringGate.set_value(true);
-    chassis.moveToPoint(48, -20, 750, {.forwards=false, .maxSpeed=40});
+    chassis.moveToPoint(48,-20,750,{.forwards=false,.maxSpeed=40});
+    //line up to park
+    //was 18
+    chassis.moveToPose(-8,-67,-90,2250,{.lead=0.5,.minSpeed=127,.earlyExitRange=4},false);
+    left_dt.move(-40);
+    right_dt.move(-40);
 
-    // =========================
-    // FINAL PARK
-    // =========================
-    chassis.moveToPose(-8, -67, -90, 2250,
-        {.lead = 0.5, .minSpeed = 127, .earlyExitRange = 4}, false);
+
+    //park
+
+
 }
--- src/util/midGoalScoring.cpp
+++ src/util/midGoalScoring.cpp
@@ -7,10 +7,11 @@
 
 void scoreMidGoal(){
     frontIntake.move(127);
-    middleRollers.move(127);
-    scoringRoller.move(40); 
+    middleRollers.move(80);
+    scoringRoller.move(80); 
     scoringGate.set_value(false);
-    pros::delay(150);
+    pros::delay(200);
+    scoringGate.set_value(true);
     frontIntake.move(-127);
     middleRollers.move(-127);
     scoringRoller.move(-127); 
@@ -23,7 +24,7 @@ void scoreMidGoal(){
     for (int i = 0; i <= totalSteps; i++) {
         double t = (double)i / totalSteps;  
 
-        double speed = 55 - 32* t * t;
+        double speed = 55 - 22* t * t;
 
         scoringRoller.move(-(int)speed);
         pros::delay(stepDelay);
